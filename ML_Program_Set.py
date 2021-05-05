9dd09e893e6b59b758d9ce26248dbc01e0550edb
Sun Xuchen <55630755+XuchenSun@users.noreply.github.com>
2021-05-05 16:18:06 -0230

    Upload My Work

---
+++ ML_Program_Set.py
@@ -0,0 +1,44 @@
+"""
+@Class Name: classification
+@Author: Xuchen Sun
+@License: General Public License （GPL）
+@Contact: xuchens@mun.ca
+@Data: 2021-05-05
+@Version: 1.0
+@Hardware Note CPU:AMD3900
+@Hardware Note GPU: EVGA GTX1080ti
+@Software Note: Python3.6+CUDA+CUDNN+Pycharm+Tensorflow
+@ML Model Type: Dense Layers(fully connected layers)
+@ML Model Type: Saved As Model_Layers.png
+@Dataset: The Mnist Dataset
+@The iterative part: Weight in Fully Connected Layers
+"""
+import Loading_Function_Set
+import Model_Set
+import Train_Function_Set
+import os
+
+class Running_ML_Program_Set:
+    def __init__(self):
+        print("Running_ML_Program_Set  Initialized")
+        os.environ['TF_XLA_FLAGS'] = '--tf_xla_enable_xla_devices'  # enable XLA Devices
+    def running_ML_program_classification(self):
+
+        EPOCHE=10
+
+        "Use function in class to build FC model"
+        personal_model_set=Model_Set.Personal_Model_Set()
+        FC_model=personal_model_set.build_FC()
+
+        "Use function in class to load data from MNIST"
+        personal_loading_function_set=Loading_Function_Set.Personal_Loading_Function_Set()
+        x_in_train,y_in_train,x_in_test,y_in_test=personal_loading_function_set.load_data_from_mnist()
+
+
+        "Use the data to train the model "
+        train_set=Train_Function_Set.Personal_Train_Function_Set()
+        train_set.train_FC_model(FC_model,x_in_train,y_in_train,x_in_test,y_in_test,EPOCHE)
+
+
+program_set=Running_ML_Program_Set()
+program_set.running_ML_program_classification()
--- classification.py
+++ classification.py
@@ -13,23 +13,8 @@
 @Dataset: The Mnist Dataset
 @The iterative part: Weight in Fully Connected Layers
 """
-import Loading_Function_Set
-import Model_Set
-import Train_Function_Set
-
-import os
-os.environ['TF_XLA_FLAGS'] = '--tf_xla_enable_xla_devices'# enable XLA Devices
-EPOCHE=10
-
-"Use function in class to build FC model"
-personal_model_set=Model_Set.Personal_Model_Set()
-FC_model=personal_model_set.build_FC()
-
-"Use function in class to load data from MNIST"
-personal_loading_function_set=Loading_Function_Set.Personal_Loading_Function_Set()
-x_in_train,y_in_train,x_in_test,y_in_test=personal_loading_function_set.load_data_from_mnist()
+import ML_Program_Set
 
 
-"Use the data to train the model "
-train_set=Train_Function_Set.Personal_Train_Function_Set()
-train_set.train_FC_model(FC_model,x_in_train,y_in_train,x_in_test,y_in_test,EPOCHE)
+program_set=ML_Program_Set.Running_ML_Program_Set()
+program_set.running_ML_program_classification()
